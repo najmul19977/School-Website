d2f36e67ab6db793d5ce497ab43011409f86fc2f
najmul19977 <najmulkabir19977@gmail.com>
2023-11-04 15:57:17 +0600

    onather part is commit

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -1,31 +1,49 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FaEnvelope,FaPhoneAlt,FaGlobe } from "react-icons/fa";
 
 const Footer = () => {
+    const emailAddress = 'example@example.com';
+    const phoneNumber = '+1234567890';
+    const websiteUrl = 'https://www.example.com';
+    
     return (
-        <footer className="footer p-10 bg-base-200 text-base-content">
+        <footer className="footer p-10 bg-base-300 text-base-content">
             <aside>
-                <svg width="50" height="50" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd" className="fill-current"><path d="M22.672 15.226l-2.432.811.841 2.515c.33 1.019-.209 2.127-1.23 2.456-1.15.325-2.148-.321-2.463-1.226l-.84-2.518-5.013 1.677.84 2.517c.391 1.203-.434 2.542-1.831 2.542-.88 0-1.601-.564-1.86-1.314l-.842-2.516-2.431.809c-1.135.328-2.145-.317-2.463-1.229-.329-1.018.211-2.127 1.231-2.456l2.432-.809-1.621-4.823-2.432.808c-1.355.384-2.558-.59-2.558-1.839 0-.817.509-1.582 1.327-1.846l2.433-.809-.842-2.515c-.33-1.02.211-2.129 1.232-2.458 1.02-.329 2.13.209 2.461 1.229l.842 2.515 5.011-1.677-.839-2.517c-.403-1.238.484-2.553 1.843-2.553.819 0 1.585.509 1.85 1.326l.841 2.517 2.431-.81c1.02-.33 2.131.211 2.461 1.229.332 1.018-.21 2.126-1.23 2.456l-2.433.809 1.622 4.823 2.433-.809c1.242-.401 2.557.484 2.557 1.838 0 .819-.51 1.583-1.328 1.847m-8.992-6.428l-5.01 1.675 1.619 4.828 5.011-1.674-1.62-4.829z"></path></svg>
-                <p>ACME Industries Ltd.<br />Providing reliable tech since 1992</p>
+                <h1 className='text-5xl font-serif font-bold'>Our School</h1>
+                <p className='text-left mb-2 '>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum laboriosam quibusdam accusamus magnam odit, similique nostrum dolor error molestiae laudantium dolore nesciunt eaque veritatis nulla asperiores quo</p>
+                <p className='flex items-center gap-5'><FaEnvelope className='text-xl'></FaEnvelope> <Link className=' font-serif ' to={`mailto:${emailAddress}`}>Info@unico.com</Link></p>
+                <p className='flex items-center gap-5'><FaPhoneAlt className='text-xl'></FaPhoneAlt> <Link className=' font-serif' to={`tel:${phoneNumber}`}>+1234567890</Link> </p>
+                <p className='flex items-center gap-5'><FaGlobe className='text-xl'></FaGlobe> <Link to={websiteUrl} target="_blank" rel="noopener noreferrer">www.unicoit.com</Link> </p>
             </aside>
             <nav>
-                <header className="footer-title">Services</header>
-                <a className="link link-hover">Branding</a>
-                <a className="link link-hover">Design</a>
-                <a className="link link-hover">Marketing</a>
-                <a className="link link-hover">Advertisement</a>
+                <header className="footer-title">Navigation </header>
+                <Link to="/" className="link link-hover">Message From Chairman</Link>
+                <Link to='/' className="link link-hover">Message From Principal</Link>
+                <Link to='/' className="link link-hover">Governing Body</Link>
+                <Link to='/' className="link link-hover">Teacher</Link>
+                
+               
             </nav>
             <nav>
-                <header className="footer-title">Company</header>
-                <a className="link link-hover">About us</a>
-                <a className="link link-hover">Contact</a>
-                <a className="link link-hover">Jobs</a>
-                <a className="link link-hover">Press kit</a>
+                <header className="footer-title">Important Links</header>
+                <Link to='/' className="link link-hover">About us</Link>
+                <Link to='/' className="link link-hover">Contact</Link>
+                <Link to='/' className="link link-hover">Noticebord</Link>
+                <Link to='/' className="link link-hover">Academic Calender</Link>
+                <Link to='/' className="link link-hover">Facilities</Link>
+                <Link to='/' className="link link-hover">Health Care</Link>
+                <Link to='/' className="link link-hover">Deess Code</Link>
+                <Link to='/' className="link link-hover">Branch</Link>
+                <Link to='/' className="link link-hover">Library</Link>
+                <Link to='/' className="link link-hover">Courses</Link>
+                
             </nav>
-            <nav>
-                <header className="footer-title">Legal</header>
-                <a className="link link-hover">Terms of use</a>
-                <a className="link link-hover">Privacy policy</a>
-                <a className="link link-hover">Cookie policy</a>
+            <nav className='w-full'>
+                <header className="footer-title">Google Map</header>
+                <div className='w-full'>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3650.874887033629!2d90.41394687447229!3d23.787469187353892!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c71ce580cc5b%3A0x628f58a3f261f98f!2sUnico%20IT%20Limited!5e0!3m2!1sen!2sbd!4v1698910892821!5m2!1sen!2sbd" className='h-72 w-full' allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
             </nav>
         </footer>
     );
